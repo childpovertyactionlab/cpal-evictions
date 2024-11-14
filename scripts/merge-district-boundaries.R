076db9e3e1da6779b08ff861f895f15d719f97d7
lopezmichael <68875922+lopezmichael@users.noreply.github.com>
2024-11-14 13:43:17 -0600

    Revert "changing to 4 characters for school ids"
    This reverts commit 5d25805092f2cddd35dcf2b4d20460b0b14125eb.

--- scripts/merge-district-boundaries.R
+++ scripts/merge-district-boundaries.R
@@ -116,8 +116,7 @@ st_write(boundary_final, "data/geographies/all_school_boundaries.geojson", delet
 
 elem_bounds <- boundary_final %>%
   filter(str_detect(unique_id, "1-|9-")) %>%
-  mutate(unique_id = str_replace(unique_id, "9-99", "9-10")) %>%
-  mutate(unique_id = paste0(1, sprintf("%03d", sample(0:999, n(), replace = FALSE))))
+  mutate(unique_id = str_replace(unique_id, "9-99", "9-10"))
 
 plot(elem_bounds["unique_id"])
 
@@ -125,8 +124,7 @@ st_write(elem_bounds, "data/geographies/elem_boundaries.geojson", delete_dsn = T
 
 mid_bounds <- boundary_final %>%
   filter(str_detect(unique_id, "2-|9-")) %>%
-  mutate(unique_id = str_replace(unique_id, "9-99", "9-20")) %>%
-  mutate(unique_id = paste0(2, sprintf("%03d", sample(0:999, n(), replace = FALSE))))
+  mutate(unique_id = str_replace(unique_id, "9-99", "9-20"))
 
 plot(mid_bounds["unique_id"])
 
@@ -134,8 +132,7 @@ st_write(mid_bounds, "data/geographies/mid_boundaries.geojson", delete_dsn = TRU
 
 high_bounds <- boundary_final %>%
   filter(str_detect(unique_id, "3-|9-")) %>%
-  mutate(unique_id = str_replace(unique_id, "9-99", "9-30")) %>%
-  mutate(unique_id = paste0(3, sprintf("%03d", sample(0:999, n(), replace = FALSE))))
+  mutate(unique_id = str_replace(unique_id, "9-99", "9-30"))
 
 plot(high_bounds["unique_id"])
 
