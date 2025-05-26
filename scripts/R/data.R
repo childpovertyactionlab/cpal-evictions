cf29057651c9597a0e0991204adbf8deb3119d7e
Ben Lake <ben@cpal.org>
2025-05-26 17:39:06 +0000

    Add automatic initialization of project directories and a helper function for scripts to initialize their own data-directories.

--- scripts/R/data.R
+++ scripts/R/data.R
@@ -18,3 +18,13 @@ project_file$master$weekly = list(
   parquet = paste0(project_file$master$weekly, ".parquet"),
   csv = paste0(project_file$master$weekly, ".csv")
 )
+
+project_init_dirs <- function (dir_list) {
+  for (dirName in names(dir_list)) {
+    if (!dir.exists(dir_list[[dirName]])) {
+      print(paste('Initializing directory', dirName, dir_list[[dirName]]))
+      dir.create(dir_list[[dirName]])
+    }
+  }
+}
+project_init_dirs(project_dir)
